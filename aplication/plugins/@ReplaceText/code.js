58fac64728192bfafd88c256f2d366ff5826e8a6
Brayan <68491457+lokuedo5000@users.noreply.github.com>
2021-04-16 18:33:13 -0500

    Add files via upload

---
+++ aplication/plugins/@ReplaceText/code.js
@@ -0,0 +1,60 @@
+/*!
+ * @ReplaceText v1
+ * Copyright 2021 @lokuedo5000
+ */
+ 'use strict'
+
+ // Define the module
+ const RemplaceAll = {
+ 	findall
+ }
+function findall(rem) {
+  return rem.replace(/::h1;/g, '<h1>')
+    .replace(/::\/h1;/g, '</h1>')
+    .replace(/::h1-mb-1;/g, '<h1 clsss="mb-1">')
+    .replace(/::h1-mb-2;/g, '<h1 clsss="mb-2">')
+    .replace(/::h1-mb-3;/g, '<h1 clsss="mb-3">')
+    .replace(/::h1-mb-4;/g, '<h1 clsss="mb-4">')
+    .replace(/::h1-mb-5;/g, '<h1 clsss="mb-5">')
+    .replace(/::h1-mb-0;/g, '<h1 clsss="mb-0">')
+    .replace(/::h2;/g, '<h2>')
+    .replace(/::\/h2;/g, '</h2>')
+    .replace(/::h2-mb-1;/g, '<h2 clsss="mb-1">')
+    .replace(/::h2-mb-2;/g, '<h2 clsss="mb-2">')
+    .replace(/::h2-mb-3;/g, '<h2 clsss="mb-3">')
+    .replace(/::h2-mb-4;/g, '<h2 clsss="mb-4">')
+    .replace(/::h2-mb-5;/g, '<h2 clsss="mb-5">')
+    .replace(/::h2-mb-0;/g, '<h2 clsss="mb-0">')
+    .replace(/::h3;/g, '<h3>')
+    .replace(/::\/h3;/g, '</h3>')
+    .replace(/::h3-mb-1;/g, '<h3 clsss="mb-1">')
+    .replace(/::h3-mb-2;/g, '<h3 clsss="mb-2">')
+    .replace(/::h3-mb-3;/g, '<h3 clsss="mb-3">')
+    .replace(/::h3-mb-4;/g, '<h3 clsss="mb-4">')
+    .replace(/::h3-mb-5;/g, '<h3 clsss="mb-5">')
+    .replace(/::h3-mb-0;/g, '<h3 clsss="mb-0">')
+    .replace(/::h4;/g, '<h4>')
+    .replace(/::\/h4;/g, '</h4>')
+    .replace(/::h4-mb-1;/g, '<h4 clsss="mb-1">')
+    .replace(/::h4-mb-2;/g, '<h4 clsss="mb-2">')
+    .replace(/::h4-mb-3;/g, '<h4 clsss="mb-3">')
+    .replace(/::h4-mb-4;/g, '<h4 clsss="mb-4">')
+    .replace(/::h4-mb-5;/g, '<h4 clsss="mb-5">')
+    .replace(/::h4-mb-0;/g, '<h4 clsss="mb-0">')
+    .replace(/::h5;/g, '<h5>')
+    .replace(/::\/h5;/g, '</h5>')
+    .replace(/::h5-mb-1;/g, '<h5 clsss="mb-1">')
+    .replace(/::h5-mb-2;/g, '<h5 clsss="mb-2">')
+    .replace(/::h5-mb-3;/g, '<h5 clsss="mb-3">')
+    .replace(/::h5-mb-4;/g, '<h5 clsss="mb-4">')
+    .replace(/::h5-mb-5;/g, '<h5 clsss="mb-5">')
+    .replace(/::h5-mb-0;/g, '<h5 clsss="mb-0">')
+    .replace(/::h6;/g, '<h6>')
+    .replace(/::\/h6;/g, '</h6>')
+    .replace(/::h6-mb-1;/g, '<h6 clsss="mb-1">')
+    .replace(/::h6-mb-2;/g, '<h6 clsss="mb-2">')
+    .replace(/::h6-mb-3;/g, '<h6 clsss="mb-3">')
+    .replace(/::h6-mb-4;/g, '<h6 clsss="mb-4">')
+    .replace(/::h6-mb-5;/g, '<h6 clsss="mb-5">')
+    .replace(/::h6-mb-0;/g, '<h6 clsss="mb-0">');
+}
